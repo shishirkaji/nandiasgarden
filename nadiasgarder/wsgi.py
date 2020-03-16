1f2a84d80c318eb5af6618182f31f3ccfa92c984
shishir <shishirkaji@gmail.com>
2020-03-16 15:45:19 +1100

    completed

---
+++ nadiasgarder/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nadiasgarder project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nadiasgarder.settings')
+
+application = get_wsgi_application()
